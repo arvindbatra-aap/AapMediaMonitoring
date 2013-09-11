4ce87adc066be81cd4884591be88c24819f5ffca
Nikesh Garera <ngarera.aap@gmail.com>
2013-09-12 03:59:42 +0530

    fixes after testing integration on linode box

--- extractor/extractor/modules/content.py
+++ extractor/extractor/modules/content.py
@@ -1,7 +1,8 @@
 import logging
 import urllib
 import urllib2
-import simplejson as json
+import json as simplejson
+from lxml import etree
 
 from extractor.basic import BasicExtractor
 
@@ -10,14 +11,16 @@ class ContentExtractor(BasicExtractor):
 	def __init__(self, *args, **kwargs):
 		super(ContentExtractor, self).__init__('ContentExtractor', *args, **kwargs)
 
-		def extract(self, datum):
-			url = 'http://localhost:2121/api/content'
-			req = urllib2.Request(url, json.dumps(datum))
-			req.add_header('Content-Type', 'text/plain; charset=utf-8')
-			response = urllib2.urlopen(req)
-			output = json.loads(response.read())
-			if output["status"] == "success":
-				return output["content"]
-			else:
-				return {}
-
+	def extract(self, datum):
+		input_data = {}
+		input_data["content"] = etree.tostring(datum['content'])
+		input_data["url"] = datum['url']
+		url = 'http://localhost:2121/api/content'
+		req = urllib2.Request(url, simplejson.dumps(input_data))
+		req.add_header('Content-Type', 'text/plain; charset=utf-8')
+		response = urllib2.urlopen(req)
+		output = simplejson.loads(response.read())
+		if output["status"] == "success":
+			return output["content"]
+		else:
+			return {}
