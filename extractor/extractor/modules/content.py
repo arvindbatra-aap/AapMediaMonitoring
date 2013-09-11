5bb6749a9c4352c8252852b0b2da7e6a018e87a7
Nikesh Garera <ngarera.aap@gmail.com>
2013-09-12 03:39:09 +0530

    calling generic extraction api when xpath extraction fails

--- extractor/extractor/modules/content.py
+++ extractor/extractor/modules/content.py
@@ -1,4 +1,7 @@
 import logging
+import urllib
+import urllib2
+import simplejson as json
 
 from extractor.basic import BasicExtractor
 
@@ -7,5 +10,14 @@ class ContentExtractor(BasicExtractor):
 	def __init__(self, *args, **kwargs):
 		super(ContentExtractor, self).__init__('ContentExtractor', *args, **kwargs)
 
-	def extract(self, datum):
-		return {}
+		def extract(self, datum):
+			url = 'http://localhost:2121/api/content'
+			req = urllib2.Request(url, json.dumps(datum))
+			req.add_header('Content-Type', 'text/plain; charset=utf-8')
+			response = urllib2.urlopen(req)
+			output = json.loads(response.read())
+			if output["status"] == "success":
+				return output["content"]
+			else:
+				return {}
+
