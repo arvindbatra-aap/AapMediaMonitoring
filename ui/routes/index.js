3aaefd777f489278ce945884546f3d90769fa3dd
Ankit Gupta <ankit.84@gmail.com>
2013-09-19 20:40:15 +0530

    fundoo waala dropdown

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.home = function(req, res){
-  res.render('index', { title: 'Express' });
+	var csv = require('csv');
+	csv().from.path(__dirname + '/../../../AapMediaMonitoring/candidatelist/CandidateList.csv', {})
+		.to.array( function(data){
+			res.render('index', { title: 'Express' , candidates: data});
+		});
 };
