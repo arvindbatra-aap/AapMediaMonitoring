1933406505eff0915d1fba7e57bcf853f7bc5954
ashriva <ashrivastava@walmartlabs.com>
2013-09-20 01:05:29 +0530

    Awesome select all/select none in chart

--- ui/public/javascripts/aap/ui.js
+++ ui/public/javascripts/aap/ui.js
@@ -5,7 +5,10 @@ var _AAP_UI = function (context) {
 	this._TREND_LOADING_DIV = '#trend-loading';
     this._ARTICLES_LOADING_DIV = '#articles-loading';
     this._ARTICLES_CONTAINER_DIV = '#articles-container';
+    this._ARTICLE_COUNT_CHART_CONTROL_DIV = '#article-count-trend-control';
     this._ARTICLE_BOX_TEMPLATE = Handlebars.compile($("#article-box-template").html());
+
+    this._chart = null;
 };
 
 _AAP_UI.prototype.renderArticleCountChart = function(chart_data) {
@@ -29,8 +32,30 @@ _AAP_UI.prototype.renderArticleCountChart = function(chart_data) {
                 text: 'Article Count'
             }
         },
-        series: chart_data.series
+        series: chart_data.series,
+        ignoreHiddenSeries: false
 	});
+    this._chart = $(this._ARTICLE_COUNT_CHART_DIV).highcharts();
+    console.log(this._chart);
+};
+
+_AAP_UI.prototype.showAllTrendSeries = function() {
+    for (var i=0; i<this._chart.series.length; i++)
+    {
+        if(!this._chart.series[i].visible) {
+            this._chart.series[i].setVisible(true, false);
+        }
+    }
+};
+
+_AAP_UI.prototype.hideAllTrendSeries = function() {
+    console.log(this._chart);
+    for (var i=0; i<this._chart.series.length; i++)
+    {
+        if(this._chart.series[i].visible && this._chart.series[i].name != 'Total') {
+            this._chart.series[i].setVisible(false, false);
+        }
+    }
 };
 
 _AAP_UI.prototype.renderArticles = function(articles) {
@@ -47,10 +72,12 @@ _AAP_UI.prototype.renderArticles = function(articles) {
 
 _AAP_UI.prototype.hideTrendLoading = function() {
 	$(this._TREND_LOADING_DIV).hide();
+    $(this._ARTICLE_COUNT_CHART_CONTROL_DIV).show();
 };
 
 _AAP_UI.prototype.showTrendLoading = function() {
 	$(this._ARTICLE_COUNT_CHART_DIV).empty();
+    $(this._ARTICLE_COUNT_CHART_CONTROL_DIV).hide();
 	$(this._TREND_LOADING_DIV).show();
 };
 
