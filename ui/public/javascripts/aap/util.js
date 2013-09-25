72862b4a7bdb5dc24944f083b73dd6790ea6ede1
ashriva <ashrivastava@walmartlabs.com>
2013-09-25 22:46:38 +0530

    Adding a util file

---
+++ ui/public/javascripts/aap/util.js
@@ -0,0 +1,22 @@
+function empty(data)
+{
+  if(typeof(data) == 'number' || typeof(data) == 'boolean') {
+    return false;
+  }
+
+  if(typeof(data) == 'undefined' || data === null) {
+    return true;
+  }
+
+  if(typeof(data.length) != 'undefined') {
+    return data.length == 0;
+  }
+
+  for(var i in data) {
+    if(data.hasOwnProperty(i)) {
+      return false;
+    }
+  }
+  
+  return true;
+}
