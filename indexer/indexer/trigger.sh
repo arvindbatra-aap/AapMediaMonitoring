088fce9588d9f122f0978d0ebe1cb01dff247739
aanand <aanand@walmartlabs.com>
2013-09-30 13:02:33 +0530

    Fixing index triggering bug

--- indexer/indexer/trigger.sh
+++ indexer/indexer/trigger.sh
@@ -1,4 +1,4 @@
 trigDate=$1
 cd /root/prod/api/AapMediaMonitoring/indexer/indexer
 export MAVEN_OPTS="-Xmx256m -Dfile.encoding=UTF8"
-/root/packages/apache-maven-3.1.0/bin/mvn clean package exec:java -Dexec.mainClass=org.aap.monitoring.TriggerMain -Dexec.args="trigDate" 2>&1 | tee logs/$trigDate.log
+mvn exec:java  -Dlog4j.configuration=file:/root/prod/api/AapMediaMonitoring/indexer/indexer/trigger.log4j -Dexec.mainClass=org.aap.monitoring.TriggerMain -Dexec.args="$trigDate" 2>&1 | tee logs/$trigDate.log
